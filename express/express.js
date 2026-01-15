68aad7b095c335350902c3e4cd4658e66d3fdebe
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-15 19:43:05 +0530

    added router

--- express/express.js
+++ express/express.js
@@ -1,1 +1,2 @@
 //it gives you routing, middleware, req and res handling
+//get, post, put, delete requests
--- express/server.js
+++ express/server.js
@@ -3,8 +3,22 @@ import express from 'express';
 const app = express();//new express application
 const port = 3000;
 
+const router = express.Router();
+
+app.get('/api/v1/cars', (req, res) => {
+    res.send('all cars');
+})
+
+//after using router
+
+router.get('/', (req, res) => {
+    res.send('All cars');
+})
+
 app.get('/', (req, res) => {
     res.end('server is running');
 })
+
+app.use('/api/v1', router);//defining router
 
 app.listen(port, () => console.log(`server is running at http://localhost:${port}`));
