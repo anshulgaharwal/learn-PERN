0bd0f2218a1a10677deab110a8fed1a60e997a38
anshulgaharwal <anshulgaharwal@gmail.com>
2026-01-16 16:37:37 +0530

    get by id

--- express/server.js
+++ express/server.js
@@ -5,20 +5,45 @@ const port = 3000;
 
 const router = express.Router();
 
-app.get('/api/v1/cars', (req, res) => {
-    res.send('all cars');
+app.use(express.json());//pars json
+
+let cars = [
+    {id: 3, make: 'Ford', model: 'F-150', year: 2021, price: 35000},
+    {id: 3, make: 'Ford', model: 'F-150', year: 2021, price: 35000},
+    {id: 3, make: 'Ford', model: 'F-150', year: 2021, price: 35000},
+]
+
+// app.get('/api/v1/cars', (req, res) => {
+//     res.send('all cars');
+// })
+
+// //after using router
+
+// router.get('/', (req, res) => {
+//     res.send('All cars');
+// })
+
+// app.get('/', (req, res) => {
+//     res.end('server is running');
+// })
+
+// router.get('/', (req, res) => {
+//     res.json(cars);//returns json
+// })
+
+//get by id
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id);
+    const car = cars.find((car) => car.id === id);
+    if(!car){
+        return res.status(404).send('Car not found');
+    }
+    return res.json(car);
 })
 
-//after using router
-
-router.get('/', (req, res) => {
-    res.send('All cars');
-})
-
-app.get('/', (req, res) => {
-    res.end('server is running');
-})
-
-app.use('/api/v1', router);//defining router
+app.use('/api/v1/cars', router);//defining router
 
 app.listen(port, () => console.log(`server is running at http://localhost:${port}`));
+
+//req.params.id returns string by deafult
